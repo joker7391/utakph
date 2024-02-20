718138fa7dfca4adf847f3601ddbfb992e4a710c
joker7391 <danielaco3091@gmail.com>
2024-02-20 15:18:36 +0800

    Fourth Commit

--- src/component/FastFoodForm.js
+++ src/component/FastFoodForm.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { FaUpload } from "react-icons/fa";
-import { fieldConfig } from "../../assets/data";
 
 import PreviewModal from "./PreviewModal";
 
 const FastFoodForm = () => {
+  const fieldConfig = [
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: ["Burgers", "Fries", "Drinks", "Desserts"],
+      required: true,
+    },
+    { name: "name", label: "Name", type: "text", required: true },
+    {
+      name: "options",
+      label: "Options (if applicable)",
+      type: "select",
+      options: ["None", "Small", "Medium", "Large"],
+    },
+    { name: "price", label: "Price", type: "number", required: true },
+    { name: "cost", label: "Cost", type: "number", required: true },
+    { name: "stock", label: "Amount in Stock", type: "number", required: true },
+  ];
+
   const [formData, setFormData] = useState({
     category: "",
     name: "",
